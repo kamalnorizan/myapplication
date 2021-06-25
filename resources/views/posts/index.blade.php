f9bf0ed04d4460abffb0627d3293a2d68a85b308
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2021-06-25 09:49:40 +0800

    comments

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,7 +10,7 @@
     <div class="row justify-content-center mt-3">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ $post->title }}</div>
+                <div class="card-header">{{ $post->title }} ~ <i>{{ $post->user->name }}</i></div>
 
                 <div class="card-body">
                     {{ $post->description }}
@@ -18,12 +18,18 @@
 
                 <div class="card-footer">
                     <h5>Comment:</h5>
+                        <strong>{{ $post->comments->first()->user->name ?? '' }}</strong><br>
+                        {{ $post->comments->first()->content ?? '' }}
+                        <hr>
                 </div>
             </div>
         </div>
     </div>
     @endforeach
+    <br>
+    {{ $posts->links('pagination::bootstrap-4') }}
 </div>
+
 @endsection
 
 @section('script')
